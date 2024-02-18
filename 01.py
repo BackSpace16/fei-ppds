ac302ac20ca356ddba9d5182127efe46c49dea90
Michal Vizváry <vizvarym1661@gmail.com>
2024-02-18 18:11:42 +0100

    feat: implement calling

--- 01.py
+++ 01.py
@@ -3,6 +3,14 @@ from random import randint
 from time import sleep
 
 
+class Shared:
+    """Class Shared is used for sharing a value among multiple threads."""
+
+    def __init__(self):
+        """Initializes the calling to False."""
+        self.calling = False
+
+
 def sleeping(name):
     """Simulate sleeping with random durration between 3 to 6 seconds."""
     time = randint(3, 6)
@@ -19,6 +27,26 @@ def hygiene(name):
     print(f"{name} finished his morning hygiene.")
 
 
+def call(shared, name):
+    """Simulate calling and waiting for response."""
+    shared.calling = True
+    print(f"{name} is calling.")
+    if (shared.calling):
+        print(f"{name} waiting for a response.")
+    while (shared.calling):
+        pass
+
+
+def recieve_call(shared, name):
+    """Simulate waiting for call and answering."""
+    if (not shared.calling):
+        print(f"{name} waiting for a call.")
+    while (not shared.calling):
+        pass   
+    shared.calling = False
+    print(f"{name} recieved call.")
+
+
 def eating(name):
     """Simulate eating with random durration between 2 to 3 seconds."""
     time = randint(2, 3)
@@ -27,11 +55,15 @@ def eating(name):
     print(f"{name} finished eating.")
 
 
-def tasks(thread_id, name):
+def tasks(shared, thread_id, name):
     """"""
     sleeping(name)
     hygiene(name)
+    if thread_id > 0:
+        recieve_call(shared, name)
     eating(name)
+    if thread_id == 0:
+        call(shared, name)
 
 
 def main():
@@ -39,7 +71,8 @@ def main():
     n_threads = 2
     names = ["Jano","Fero"]
 
-    threads = [Thread(tasks, i, names[i]) for i in range(n_threads)]
+    shared = Shared()
+    threads = [Thread(tasks, shared, i, names[i]) for i in range(n_threads)]
     [t.join() for t in threads]
 
 
