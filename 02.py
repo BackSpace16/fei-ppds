26bdd92df19c97727e590269c66c0ba4adab5dda
Michal Vizváry <vizvarym1661@gmail.com>
2024-02-21 21:02:59 +0100

    feat: add pot, portions taking and refill logic

--- 02.py
+++ 02.py
@@ -4,6 +4,7 @@ from fei.ppds import Thread, Mutex, Semaphore
 
 
 N_DINER = 7
+POT_CAPACITY = 5
 
 
 class SimpleBarrier:
@@ -26,25 +27,40 @@ class Shared:
     """Shared class among multiple threads."""
     def __init__(self):
         """Initialise."""
+        self.pot = Mutex()
+        self.portions = 0
         self.barrier = SimpleBarrier(N_DINER)
-        # TODO implement
-        pass
+        self.chef = Semaphore(0)
+        self.chef_done = Semaphore(0)
 
 
 def diner(thread_id, shared):
     """Diner consumes dinner."""
-    # TODO implement
     sleep(randint(1,10))
-    print(f"{thread_id} dorazil k bariere")
+    print(f"{thread_id} dorazil k obedu")
     shared.barrier.wait()
     print(f"{thread_id} vypustili")
-    pass
+    
+    shared.pot.lock()
+    if shared.portions > 0:
+        print(f"{thread_id} si bere jedlo")
+        shared.portions -= 1
+    else:
+        print(f"{thread_id} zistil že hrniec je prázdny, volá kuchárovi")
+        shared.chef.signal()
+        shared.chef_done.wait()
+        print(f"{thread_id} si bere jedlo")
+        shared.portions -= 1
+    shared.pot.unlock()
 
 
 def chef(shared):
     """Chef cooks meal for diners."""
-    # TODO implement
-    pass
+    while 42:
+        shared.chef.wait()
+        shared.portions += POT_CAPACITY
+        print("kuchar doplnil hrniec")
+        shared.chef_done.signal()
 
 
 def main():
