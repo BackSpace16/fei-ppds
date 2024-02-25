6117c9377708fecb770ec4faf9f775b31711e627
Michal Vizváry <vizvarym1661@gmail.com>
2024-02-25 16:58:31 +0100

    feat: add console output

--- 02.py
+++ 02.py
@@ -13,6 +13,10 @@ class SimpleBarrier:
         self.turnstile = Semaphore(0)
         self.max_threads = max_threads
         self.counter = 0
+        self.unlock_text = "Barrier unlocked."
+
+    def set_unlock_text(self, text):
+        self.unlock_text = text
 
     def wait(self):
         self.mutex.lock()
@@ -20,6 +24,7 @@ class SimpleBarrier:
         if self.counter == self.max_threads:
             self.turnstile.signal(self.max_threads)
             self.counter = 0
+            print(self.unlock_text)
         self.mutex.unlock()
         self.turnstile.wait()
 
@@ -27,11 +32,13 @@ class SimpleBarrier:
 class Shared:
     """Shared class among multiple threads."""
     def __init__(self):
-        """Initialise."""
+        """Initialise shared variables."""
         self.pot = Mutex()
-        self.portions = 0
+        self.portions = 10
         self.barrier = SimpleBarrier(N_DINER)
+        self.barrier.set_unlock_text("All diners came to lunch.")
         self.barrier2 = SimpleBarrier(N_DINER)
+        self.barrier2.set_unlock_text("All diners have their portion. They proceed to eat.")
         self.chef = Semaphore(0)
         self.chef_done = Semaphore(0)
 
@@ -40,31 +47,34 @@ def diner(thread_id, shared):
     """Diner consumes dinner."""
     while 16:
         sleep(randint(1,10))
-        print(f"{thread_id} dorazil k obedu")
+        print(f"Diner {thread_id} came to lunch.")
         shared.barrier.wait()
-        print(f"{thread_id} vypustili")
 
         shared.pot.lock()
         if shared.portions > 0:
-            print(f"{thread_id} si bere jedlo")
+            print(f"Diner {thread_id} picked his portion.")
             shared.portions -= 1
         else:
-            print(f"{thread_id} zistil že hrniec je prázdny, volá kuchárovi")
+            print(f"Diner {thread_id} realised the pot is empty. Calling chef.")
             shared.chef.signal()
             shared.chef_done.wait()
-            print(f"{thread_id} si bere jedlo")
+            print(f"Diner {thread_id} picked his portion.")
             shared.portions -= 1
         shared.pot.unlock()
 
         shared.barrier2.wait()
+        sleep(5)
+        print(f"Diner {thread_id} finished lunch.")
 
 
 def chef(shared):
     """Chef cooks meal for diners."""
     while 42:
         shared.chef.wait()
+        print("Chef is preparing food.")
+        sleep(5)
         shared.portions += POT_CAPACITY
-        print("kuchar doplnil hrniec")
+        print("Chef refilled the pot.")
         shared.chef_done.signal()
 
 
