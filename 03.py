7c020d85af938565395a6a1d21e4431b50f4cb5a
Michal Vizváry <vizvarym1661@gmail.com>
2024-03-04 18:42:12 +0100

    feat: basic structure

--- 03.py
+++ 03.py
@@ -1,8 +1,31 @@
 from fei.ppds import Thread
 
 
+N_PASSENGERS = 20
+
+
+class Shared:
+    """Shared class among multiple threads."""
+    def __init__(self):
+        """Initialise shared variables."""
+
+
+def train(id, shared):
+    """ TODO """
+    pass
+
+
+def passenger(id, shared):
+    """ TODO """
+    pass
+
+
 def main():
     """."""
+    shared = Shared()
+    threads = [Thread(passenger, i, shared) for i in range(N_PASSENGERS)]
+    threads.append(Thread(train, shared))
+    [t.join() for t in threads]
 
 
 if __name__ == '__main__':
