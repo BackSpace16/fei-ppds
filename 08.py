0236e30840b87945331032ff2b0cfd71cd6c276d
Michal Vizváry <vizvarym1661@gmail.com>
2024-04-12 14:00:47 +0200

    feat: use numpy arrays

--- 08.py
+++ 08.py
@@ -1,36 +1,41 @@
 import random
+import numpy as np
 
 
+LIST_LENGTH = 10
+MIN_VALUE = 0
+MAX_VALUE = 100
 N_BUCKETS = 4
 
 
 def sample_sort(data, p):
 
     # choose and sort samples
-    samples = random.sample(data, p-1)
+    samples = np.random.choice(data, p-1, replace=False)
     samples.sort()
-    splitters = [-float('inf')] + samples + [float('inf')]
+    splitters = np.concatenate(([MIN_VALUE-1], samples, [MAX_VALUE+1]))
+    print(splitters)
 
     # create buckets from samples
-    buckets = [[] for _ in range(len(splitters) - 1)]
+    buckets = [np.empty(0, dtype=int) for _ in range(len(splitters) - 1)]
     
     for a in data:
         j = 0
         while not (splitters[j] < a <= splitters[j+1]):
             j += 1
-        buckets[j].append(a)
+        buckets[j] = np.append(buckets[j], a)
 
     # sort buckets
     for b in buckets:
         b.sort()
 
     # concentrate buckets
-    data = [item for sublist in buckets for item in sublist]
+    data = np.concatenate(buckets)
     return data
 
 
 def main():
-    list = [2,5,1,4,9,7]
+    list = np.random.randint(MIN_VALUE, MAX_VALUE, size=LIST_LENGTH)
     print(list)
 
     sorted_list = sample_sort(list, N_BUCKETS)
