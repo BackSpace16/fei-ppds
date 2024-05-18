d5c3d850537e86bd4b9cbdcdbd3f75f783b24d67
Michal Vizváry <vizvarym1661@gmail.com>
2024-05-18 20:17:58 +0200

    fix: support all n_proc and n_nodes values fixed parallel part not working for n_nodes % n_proc != 0 and n_proc > n_nodes values

--- sz_par.py
+++ sz_par.py
@@ -14,29 +14,64 @@ def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     nproc = comm.Get_size()
-    
+
+    nproc_split = False
+    if nproc > N_NODES:
+        nproc_split = True
+        comm = MPI.COMM_WORLD.Split(color=(rank < N_NODES))
+        nproc = comm.Get_size()
+
+    indices_modulo = N_NODES % nproc
+    comm_tail = MPI.COMM_WORLD.Split(color=(rank < indices_modulo))
+
     indices = None
+    indices_tail = None
     adj_matrix = None
 
     if rank == MASTER:
         start_time = MPI.Wtime()
-        indices = np.array([i for i in range(N_NODES)])
+
         adj_matrix = generate_adj_matrix(N_NODES, MIN_WEIGHT, MAX_WEIGHT, EDGE_DENSITY)
+        indices = np.array([i for i in range(N_NODES)])
+
+        if indices_modulo != 0:
+            indices_tail = indices[-(N_NODES % nproc) :]
+            indices = indices[: -(N_NODES % nproc)]
+            indices_tail = indices_tail.reshape(indices_modulo, 1)
+
         indices = indices.reshape(nproc, N_NODES // nproc)
+
+    indices_loc = comm.scatter(indices, root=MASTER)
+    if indices_modulo != 0 and rank < indices_modulo:
+        indices_loc_tail = comm_tail.scatter(indices_tail, root=MASTER)
+    adj_matrix = comm.bcast(adj_matrix, root=MASTER)
+
+    if (not nproc_split or 
+            nproc_split and nproc - N_NODES == 0 and rank < nproc):
+
+        distances = []
+        for i in indices_loc:
+            distances.append(dijkstra(adj_matrix, i))
+
+        distances = np.array(distances)
+        par_distances = comm.gather(distances, root=MASTER)
         
-    indices_loc = comm.scatter(indices, root = MASTER)
-    adj_matrix = comm.bcast(adj_matrix, root = MASTER)
+        if indices_modulo != 0 and rank < indices_modulo:
+            
+            distances_tail = []
+            for i in indices_loc_tail:
+                distances_tail.append(dijkstra(adj_matrix, i))
 
-    distances = []
-    for i in indices_loc:
-        distances.append(dijkstra(adj_matrix, i))
+            distances_tail = np.array(distances_tail)
+            distances_tail = comm_tail.gather(distances_tail, root=MASTER)
 
-    distances = np.array(distances)
-    par_distances = comm.gather(distances, root = MASTER)
+            if rank == MASTER:
+                distances_tail = np.array([ss for s in distances_tail for ss in s])
     
     if rank == MASTER:
         par_distances = np.array([ss for s in par_distances for ss in s])
-        par_distances = par_distances.reshape(N_NODES, N_NODES)
+        if indices_modulo != 0:
+            par_distances = np.vstack([par_distances, distances_tail])
 
         end_time = MPI.Wtime()
         elapsed_time = end_time - start_time
